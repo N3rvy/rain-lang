a3aed45fab1ad5f601d4f33d8cea18d61c54a08d
N3rvy <manuelmanuel2003@gmail.com>
2022-01-12 09:49:41 +0100

    REPL wont quit anymore if founds an error

--- repl/src/main.rs
+++ repl/src/main.rs
@@ -1,15 +1,16 @@
 use std::io::{BufRead, stdin};
-use reverse::{Vm, IntoScript, LangError};
+use reverse::{Vm, IntoScript};
 
 
-fn main() -> Result<(), LangError> {
+fn main() {
     let vm = Vm::new();
     
     for script in stdin().lock().lines() {
         if let Ok(script) = script {
-            println!("{:?}", vm.evaluate(&script.script()?));
+            match script.script() {
+                Ok(script) => println!("{:?}", vm.evaluate(&script)),
+                Err(err) => println!("{}", err),
+            }
         }
     }
-    
-    Ok(())
 }
