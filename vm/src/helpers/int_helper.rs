0bff4aa5bdc569cbd61ee1333aac3093c9ebaa19
N3rvy <manuelmanuel2003@gmail.com>
2022-01-15 02:03:00 +0100

    Removed int add helper (useless...)

--- vm/src/helpers/int_helper.rs
+++ vm/src/helpers/int_helper.rs
@@ -1,17 +1,10 @@
 use common::helper::Helper;
-use common::external_functions::IntoExternalFunctionRunner;
-
 
 pub fn default_int_helper() -> Helper {
     let mut helper = Helper::new();
 
     helper.register("max", i32::MAX);
     helper.register("min", i32::MIN);
-    helper.register("add", add.external());
-        
-    helper
-}
 
-fn add(a: i32, b: i32) -> i32 {
-    a + b
+    helper
 }
