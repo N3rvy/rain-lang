4ef8ce962155d598c997c824227f795a6c5ddf98
N3rvy <manuelmanuel2003@gmail.com>
2022-06-01 08:35:16 +0200

    Adding modules to portal-wasm

--- portal-wasm-build/src/lib.rs
+++ portal-wasm-build/src/lib.rs
@@ -4,27 +4,62 @@ use common::module::{ModuleIdentifier, ModuleUID};
 use wasm::engine::WasmEngine;
 use wasm_bindgen::prelude::*;
 
+static mut MODULES: Vec<(String, String)> = Vec::new();
+static mut ENGINE: Option<WasmEngine> = None;
+
+#[wasm_bindgen]
+pub fn add_module(id: &str, module: &str) {
+    unsafe {
+        MODULES.push((id.to_string(), module.to_string()));
+    }
+}
+
+#[wasm_bindgen]
+pub fn init_engine() {
+    unsafe {
+        let mut engine = WasmEngine::new();
+
+        engine.module_loader()
+            .load_module_with_source(
+                ModuleIdentifier("core".to_string()),
+                ModuleUID::from_string("core".to_string()),
+                &include_str!("../../core_lib/lib.vrs").to_string(),
+                &PanicImporter,
+            ).expect("Error while loading core module :-|");
+
+        for (id, module) in &MODULES {
+            engine.module_loader()
+                .load_module_with_source(
+                    ModuleIdentifier(id.to_string()),
+                    ModuleUID::from_string(id.to_string()),
+                    module,
+                    &PanicImporter,
+                ).expect("Error while loading imported module :-|");
+        }
+
+        ENGINE = Some(engine);
+    }
+}
+
 #[wasm_bindgen]
 pub fn build_from_code(code: &str) -> Vec<u8> {
-    let mut engine = WasmEngine::new();
+    unsafe {
+        let engine = match &mut ENGINE {
+            Some(engine) => engine,
+            None => panic!("Engine not initialized"),
+        };
 
-    engine.module_loader()
-        .load_module_with_source(
-            ModuleIdentifier("core".to_string()),
-            ModuleUID::from_string("core".to_string()),
-            &include_str!("../../core_lib/lib.vrs").to_string(),
-            &PanicImporter,
-        ).expect("Error while loading core module :-|");
+        let (module, _) = engine
+            .module_loader()
+            .load_module_with_source(
+                ModuleIdentifier("main".to_string()),
+                ModuleUID::from_string("main".to_string()),
+                &code.to_string(),
+                &PanicImporter
+            ).unwrap();
 
-    let (module, _) = engine.module_loader()
-        .load_module_with_source(
-            ModuleIdentifier("main".to_string()),
-            ModuleUID::from_string("main".to_string()),
-            &code.to_string(),
-            &PanicImporter
-        ).unwrap();
-
-    engine.build_module_source(module.uid).unwrap()
+        engine.build_module_source(module.uid).unwrap()
+    }
 }
 
 struct PanicImporter;
